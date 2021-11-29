e25c215de6831d95be528bae8cc4819ae852a8c6
Saxbeck <saxbeck.patrick@gmail.com>
2021-11-29 08:17:21 +0100

    Initial Commit

---
+++ Shared/VelfaerdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  VelfaerdsApp.swift
+//  Shared
+//
+//  Created by Patrick Saxbeck on 29/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct VelfaerdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
